92772e56db2c275b1b9008ffb836c4e78824494b
0xAA55 <weixiaobao213@qq.com>
2025-03-18 19:58:52 +0800

    设计接口用于适应各种各样的音频文件解析器。

--- src/audioreader.rs
+++ src/audioreader.rs
@@ -1,10 +1,12 @@
-use std::error::Error;
+use std::{error::Error};
 
 #[derive(Debug)]
 pub enum AudioReadError {
     IOError(String), // 读写错误，应停止处理
     FormatError, // 格式错误，说明可以尝试使用别的格式的读取器来读取
+    DataCorrupted, // 格式也许是正确的，但是数据是错误的
     Unimplemented, // 格式正确，但是这种格式的文件的读写方式没有被开发出来，应停止处理
+    EndOfFile, // 超出文件结尾
 }
 
 impl Error for AudioReadError {}
@@ -14,12 +16,14 @@ impl std::fmt::Display for AudioReadError {
        match self {
            AudioReadError::IOError(error) => write!(f, "IOError {error}"),
            AudioReadError::FormatError => write!(f, "Invalid audio file format"),
+           AudioReadError::DataCorrupted => write!(f, "Audio file data corrupted"),
            AudioReadError::Unimplemented => write!(f, "Unimplemented for the file format"),
+           AudioReadError::EndOfFile => write!(f, "Read to the end of the file"),
        }
     }
 }
 
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 pub enum SampleFormat {
     Unknown,
     Float,
@@ -45,19 +49,35 @@ impl Spec {
     }
 }
 
-pub struct Frame(f32, f32);
+pub struct Frame(pub f32, pub f32);
 
-pub trait AudioReader: Iterator<Item = Frame> {
-    fn open(input_file: &str) -> Result<Self, Box<dyn Error>> where Self: Sized {
-        panic!("Abstract function called with param input_file = {input_file}.");
-    }
+pub trait AudioReader {
     fn spec(&self) -> Spec;
 
-    fn iter(&mut self) -> AudioReaderIter;
+    fn get_sample(&mut self, position: u64) -> Result<Frame, Box<dyn Error>>;
+
+    fn iter(&mut self) -> AudioReaderIter where Self: Sized {
+        AudioReaderIter{
+            reader: self,
+            position: 0,
+        }
+    }
 }
 
-pub trait AudioReaderIter: Iterator {
+pub struct AudioReaderIter<'a> {
+    reader: &'a mut dyn AudioReader,
+    position: u64,
+}
+
+impl Iterator for AudioReaderIter<'_> {
     type Item = Frame;
 
-    fn next(&mut self) -> Option<Self::Item>;
+    fn next(&mut self) -> Option<Self::Item> {
+        let query = self.position;
+        self.position += 1;
+        match self.reader.get_sample(query) {
+            Ok(frame) => Some(frame),
+            Err(_) => None,
+        }
+    }
 }
