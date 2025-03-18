698bb98ff86548600c060d4fbee2c4b061b56615
0xAA55 <weixiaobao213@qq.com>
2025-03-18 22:11:52 +0800

    将音频处理相关的公共的东西整合到一起。

---
+++ src/audiocore.rs
@@ -0,0 +1,28 @@
+
+#[derive(Copy, Clone)]
+pub enum SampleFormat {
+    Unknown,
+    Float,
+    Int,
+}
+
+#[derive(Clone)]
+pub struct Spec {
+    pub channels: u16,
+    pub sample_rate: u32,
+    pub bits_per_sample: u16,
+    pub sample_format: SampleFormat,
+}
+
+impl Spec {
+    pub fn new() -> Self {
+        Self {
+            channels: 0,
+            sample_rate: 0,
+            bits_per_sample: 0,
+            sample_format: SampleFormat::Unknown,
+        }
+    }
+}
+
+pub struct Frame(pub f32, pub f32);
