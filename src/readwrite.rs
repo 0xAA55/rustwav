b0ae0d199df0b7fdd3da4a2ae07739625fd650bc
0xAA55 <weixiaobao213@qq.com>
2025-03-25 00:14:18 +0800

    把 `Box<dyn Writer>` 包装成 `DynWriter` 后，它就认为这玩意儿支持 `std::io::Write` 了

--- src/readwrite.rs
+++ src/readwrite.rs
@@ -6,56 +6,30 @@ impl<T> Reader for T where T: Read + Seek {}
 pub trait Writer: Write + Seek{}
 impl<T> Writer for T where T: Write + Seek {}
 
-#[allow(non_camel_case_types)]
-pub struct ReaderS_Over_D {
-	reader: Box<dyn Reader>,
+struct DynWriter {
+    writer: Box<dyn Writer>,
 }
 
-impl ReaderS_Over_D {
-	fn from(reader: Box<dyn Reader>) -> Self {
-		Self {
-			reader,
-		}
-	}
-}
-
-impl Read for ReaderS_Over_D {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize, io::Error> {
-    	self.reader.read(buf)
+impl DynWriter {
+    fn new(writer: Box<dyn Writer>) -> Self {
+        Self {
+            writer,
+        }
     }
 }
 
-impl Seek for ReaderS_Over_D {
-	fn seek(&mut self, pos: SeekFrom) -> Result<u64, io::Error> {
-		self.reader.seek(pos)
-	}
+impl Seek for DynWriter {
+    fn seek(&mut self, pos: SeekFrom) -> Result<u64, io::Error> {
+        self.writer.seek(pos)
+    }
 }
 
-#[allow(non_camel_case_types)]
-pub struct WriterS_Over_D {
-	writer: Box<dyn Writer>,
-}
-
-impl WriterS_Over_D {
-	fn from(writer: Box<dyn Writer>) -> Self {
-		Self {
-			writer,
-		}
-	}
-}
-
-impl Seek for WriterS_Over_D {
-	fn seek(&mut self, pos: SeekFrom) -> Result<u64, io::Error> {
-		self.writer.seek(pos)
-	}
-}
-
-impl Write for WriterS_Over_D {
+impl Write for DynWriter {
     fn write(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
-    	self.writer.write(buf)
+        self.writer.write(buf)
     }
 
     fn flush(&mut self) -> Result<(), io::Error> {
-    	self.writer.flush()
+        self.writer.flush()
     }
 }
