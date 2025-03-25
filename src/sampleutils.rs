d3ae3a1ec841859b359dc36df45fe26ea5c1b396
0xAA55 <weixiaobao213@qq.com>
2025-03-25 20:00:44 +0800

    全面应用莽夫式字符串转换功能。

--- src/sampleutils.rs
+++ src/sampleutils.rs
@@ -2,6 +2,8 @@
 
 use std::{io::{Read, Write, Error}, mem::size_of, clone::Clone};
 
+use crate::savagestr::SavageStringDecoder;
+
 #[derive(Debug, Clone, Copy)]
 #[allow(non_camel_case_types)]
 pub struct i24(pub u8, pub u8, pub u8); // 低中高
@@ -819,25 +821,14 @@ impl SampleType for f64{
     }
 }
 
-// 莽夫式字节数组转 UTF-8 字符串，如果字节数组不是 UTF-8 字符串，那爱怎么乱码就怎么乱码吧。
-pub fn savage_bytes_to_string(bytes: &[u8]) -> String {
-    let buf = String::from_utf8_lossy(&bytes);
-    format!("{}",buf)
-}
-
-pub fn savage_flag_to_string(flag: &[u8; 4]) -> String {
-    let buf = String::from_utf8_lossy(flag);
-    format!("{}",buf)
-}
-
-pub fn read_str<T: Read>(r: &mut T, size: usize) -> Result<String, Box<dyn std::error::Error>> {
+pub fn read_str<T: Read>(r: &mut T, size: usize, savage_decoder: &SavageStringDecoder) -> Result<String, Box<dyn std::error::Error>> {
     let mut buf = Vec::<u8>::new();
     buf.resize(size, 0);
     r.read_exact(&mut buf)?;
-    Ok(savage_bytes_to_string(&buf))
+    Ok(savage_decoder.decode(&buf))
 }
 
-pub fn read_sz<T: Read>(w: &mut T) -> Result<String, Box<dyn std::error::Error>> {
+pub fn read_sz<T: Read>(w: &mut T, savage_decoder: &SavageStringDecoder) -> Result<String, Box<dyn std::error::Error>> {
     let mut buf = Vec::<u8>::new();
     loop {
         let b = u8::read_le(w)?;
@@ -847,7 +838,7 @@ pub fn read_sz<T: Read>(w: &mut T) -> Result<String, Box<dyn std::error::Error>>
             break;
         }
     }
-    Ok(savage_bytes_to_string(&buf))
+    Ok(savage_decoder.decode(&buf))
 }
 
 pub fn expect_flag<T: Read>(r: &mut T, flag: &[u8; 4], err: Box<dyn std::error::Error>) -> Result<(), Box<dyn std::error::Error>> {
