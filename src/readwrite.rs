ae2232a841e81d12b12cd22878e72d4aa14f484c
0xAA55 <weixiaobao213@qq.com>
2025-03-25 23:24:25 +0800

    给 Writer 实现通过填充 0 来使文件大小以 2 对齐。

--- src/readwrite.rs
+++ src/readwrite.rs
@@ -8,6 +8,17 @@ impl<T> Reader for T where T: Read + Seek + Debug{}
 pub trait Writer: Write + Seek+ Debug{}
 impl<T> Writer for T where T: Write + Seek + Debug{}
 
+impl Writer {
+    // 使文件的长度以 2 字节对齐。
+    fn pad(&self) -> Result<(), io::Error> {
+        let pad_size = self.stream_position()? & 1;
+        if pad_size {
+            self.write_all(&[0u8; pad_size])?
+        }
+        Ok(())
+    }
+}
+
 #[derive(Debug)]
 pub struct DynWriter {
     writer: Box<dyn Writer>,
