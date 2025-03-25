e397e5753fba6b18e86a5733de2a5a393dd09426
0xAA55 <weixiaobao213@qq.com>
2025-03-25 15:28:12 +0800

    修复一处没有处理返回错误的问题。

--- src/sampleutils.rs
+++ src/sampleutils.rs
@@ -852,7 +852,7 @@ pub fn read_sz<T: Read>(w: &mut T) -> Result<String, Box<dyn std::error::Error>>
 
 pub fn expect_flag<T: Read>(r: &mut T, flag: &[u8; 4], err: Box<dyn std::error::Error>) -> Result<(), Box<dyn std::error::Error>> {
     let mut buf = [0u8; 4];
-    r.read_exact(&mut buf);
+    r.read_exact(&mut buf)?;
     if &buf != flag {
         Err(err)
     } else {
