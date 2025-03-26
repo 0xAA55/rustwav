2de3ed664b000d07f91f19465dbc87b14febf376
0xAA55 <weixiaobao213@qq.com>
2025-03-27 01:20:05 +0800

    新增一个宏，用于剥掉 `Arc<Mutex<T>>`，放出里面的 `MutexGuard` 和你想要的 `&mut T`。

--- src/readwrite.rs
+++ src/readwrite.rs
@@ -2,41 +2,24 @@
 
 pub use std::{io::{self, Read, Write, Seek, SeekFrom}, fmt::Debug};
 
-pub trait Reader: Read + Seek + Debug{}
+pub trait Reader: Read + Seek + Debug {}
 impl<T> Reader for T where T: Read + Seek + Debug{}
 
-pub trait Writer: Write + Seek+ Debug{}
+pub trait Writer: Write + Seek + Debug {}
 impl<T> Writer for T where T: Write + Seek + Debug{}
 
-#[derive(Debug)]
-pub struct DynWriter {
-    writer: Box<dyn Writer>,
+// 这个宏的作用是：首先你有一个 Arc<Mutex<T>>，你想要使用里面的 &mut T
+// 首先 Mutex<T> 这里 lock() 释放出一个 MutexGuard，存入 $guard_name
+// 然后调用 MutexGuard 的 deref_mut() 放出 &mut T 你就可以操作其中的 T 了
+// MutexGuard 必须活着。一旦它被销毁了，T 就不能用了。
+// 这个宏会在当前 scope 里面声明两个变量，一个 $guard_name 和一个 $inner_name
+// 这个 $inner_name 就是你的 &mut T。
+#[macro_export]
+macro_rules! make_guarded_writer {
+    ($arc_mutex_dyn_writer:expr, $inner_name:ident, $guard_name:ident) => {
+        let mut $guard_name = $arc_mutex_dyn_writer.lock().unwrap();
+        let mut $inner_name = $guard_name.deref_mut();
+    };
 }
 
-impl DynWriter {
-    pub fn new(writer: Box<dyn Writer>) -> Self {
-        Self {
-            writer,
-        }
-    }
-
-    pub fn borrow_inside(&self) -> &Box<dyn Writer> {
-    	&self.writer
-    }
-}
-
-impl Seek for DynWriter {
-    fn seek(&mut self, pos: SeekFrom) -> Result<u64, io::Error> {
-        self.writer.seek(pos)
-    }
-}
-
-impl Write for DynWriter {
-    fn write(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
-        self.writer.write(buf)
-    }
-
-    fn flush(&mut self) -> Result<(), io::Error> {
-        self.writer.flush()
-    }
-}
+pub use crate::make_guarded_writer;
