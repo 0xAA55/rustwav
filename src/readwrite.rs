033c5a8af2aa31f7ef97646e0cdffa178e2bbdef
0xAA55 <weixiaobao213@qq.com>
2025-03-28 23:18:55 +0800

    将 `use_writer()` 中的函数指针换成闭包，这样它就能访问调用者的局部变量了。

--- src/readwrite.rs
+++ src/readwrite.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 
-pub use std::{io::{self, Read, Write, Seek, SeekFrom}, sync::{Arc, Mutex}, fmt::Debug, error::Error};
+pub use std::{io::{self, Read, Write, Seek, SeekFrom}, sync::{Arc, Mutex}, ops::{DerefMut}, fmt::Debug, error::Error};
+
+pub use crate::savagestr::SavageStringDecoder;
 
 pub trait Reader: Read + Seek + Debug {}
 impl<T> Reader for T where T: Read + Seek + Debug {}
@@ -8,11 +10,11 @@ impl<T> Reader for T where T: Read + Seek + Debug {}
 pub trait Writer: Write + Seek + Debug {}
 impl<T> Writer for T where T: Write + Seek + Debug {}
 
-pub fn use_writer(writer_shared: Arc<Mutex<Writer>>, action: fn(&mut Writer) -> Result<(), Box<dyn Error>>) -> Result<(), Box<dyn Error>> {
-    let writer_shared = writer_shared.clone();
+pub fn use_writer<F>(writer_shared: Arc<Mutex<dyn Writer>>, mut action: F) -> Result<(), Box<dyn Error>>
+where F: FnMut(&mut dyn Writer) -> Result<(), Box<dyn Error>> {
     let mut guard = writer_shared.lock().unwrap();
     let mut writer = guard.deref_mut();
-    (action)(&writer)
+    (action)(&mut writer)
 }
 
 pub fn expect_flag<T: Read>(r: &mut T, flag: &[u8; 4], err: Box<dyn std::error::Error>) -> Result<(), Box<dyn std::error::Error>> {
