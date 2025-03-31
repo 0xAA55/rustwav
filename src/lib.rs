1a33b2c41c524ed8f57a94af10e01da03b2d30b5
0xAA55 <weixiaobao213@qq.com>
2025-04-01 02:53:07 +0800

    将所有适合暴露出来的东西全部暴露出来。

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,15 @@ mod wavcore;
 mod wavreader;
 mod wavwriter;
 
-pub use wavcore::DataFormat;
-pub use wavreader::WaveReader;
-pub use wavwriter::{WaveWriter, Spec, FileSizeOption, SampleFormat};
+pub use errors::{AudioError, AudioReadError, AudioWriteError};
+pub use savagestr::{StringCodecMaps, SavageStringCodecs};
+pub use readwrite::{Reader, Writer, SharedWriter, StringIO};
+pub use sampleutils::{SampleType, SampleFrom, i24, u24};
+pub use encoders::{EncoderBasic, Encoder, PcmEncoder};
+pub use decoders::{Decoder, PcmDecoder};
+pub use wavcore::{DataFormat, Spec, SampleFormat, WaveSampleType, SpeakerPosition};
+pub use wavcore::{GUID, GUID_PCM_FORMAT, GUID_IEEE_FLOAT_FORMAT};
+pub use wavcore::{ChunkWriter, ChunkHeader};
+pub use wavcore::{FmtChunk, FmtChunkExtension, BextChunk, SmplChunk, SmplSampleLoop, InstChunk, CueChunk, Cue, ListChunk, AdtlChunk, LablChunk, NoteChunk, LtxtChunk, AcidChunk, JunkChunk, Id3};
+pub use wavreader::{WaveDataSource, WaveReader, WaveIter};
+pub use wavwriter::{FileSizeOption, WaveWriter};
