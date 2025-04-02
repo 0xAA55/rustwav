f74ee844fbccee727d3d28932605f13a6c59d1e2
0xAA55 <weixiaobao213@qq.com>
2025-04-03 03:22:39 +0800

    使生成常规文件头便于观察二进制内容。

--- src/test.rs
+++ src/test.rs
@@ -47,8 +47,10 @@ fn test(arg1: &str, arg2: &str) -> Result<(), Box<dyn Error>> {
         sample_format: SampleFormat::Int, // 使用有符号整数
     };
 
+    use FileSizeOption::{NeverLargerThan4GB, AllowLargerThan4GB, ForceUse4GBFormat};
+
     // 音频写入器，将音频信息写入到 arg2 文件
-    let mut wavewriter = WaveWriter::create(arg2, &spec, DataFormat::Pcm, FileSizeOption::ForceUse4GBFormat).unwrap();
+    let mut wavewriter = WaveWriter::create(arg2, &spec, DataFormat::Pcm, NeverLargerThan4GB).unwrap();
 
     // 使用迭代器读取 WaveReader 的音频，注意迭代器支持一个泛型参数，此处设置的是 f32
     // 迭代器会自动把读取到的原始音频格式按照这个泛型格式做转换，并使样本的数值符合样本数据类型的范围
