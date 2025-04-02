adb5696ac154082891280b816bea7dc08663550d
0xAA55 <weixiaobao213@qq.com>
2025-04-03 00:30:34 +0800

    恢复编码器本身的实现，准备重新尝试其它的策略来优化代码。
    This reverts commit df8e016423f4a77d89e0460ee85dd30a9e995eaf.

--- src/test.rs
+++ src/test.rs
@@ -19,7 +19,7 @@ pub use errors::{AudioError, AudioReadError, AudioWriteError};
 pub use savagestr::{StringCodecMaps, SavageStringCodecs};
 pub use readwrite::{Reader, Writer, SharedWriter, string_io};
 pub use sampleutils::{SampleType, SampleFrom, i24, u24};
-pub use encoders::{EncoderFrom, Encoder, PcmEncoder};
+pub use encoders::{EncoderBasic, Encoder, PcmEncoder};
 pub use decoders::{Decoder, PcmDecoder};
 pub use wavcore::{DataFormat, Spec, SampleFormat, WaveSampleType, SpeakerPosition};
 pub use wavcore::{GUID, GUID_PCM_FORMAT, GUID_IEEE_FLOAT_FORMAT};
