02b11290aff82dfb7a1931b12bc3e28107da7f0e
0xAA55 <weixiaobao213@qq.com>
2025-04-05 23:13:13 +0800

    `AdpcmEncoder::<T>::new()` 是不会报错的。

--- src/wavwriter.rs
+++ src/wavwriter.rs
@@ -73,7 +73,7 @@ impl WaveWriter {
                 Encoder::new(PcmEncoder::new(spec.channels, spec.sample_rate, sample_type)?)
             },
             Adpcm => {
-                Encoder::new(AdpcmEncoder::<EncBS>::new(spec.sample_rate)?)
+                Encoder::new(AdpcmEncoder::<EncBS>::new(spec.sample_rate))
             },
             Mp3 => {
                 Encoder::new(Mp3Encoder::<f32>::new(spec.channels as u8, spec.sample_rate, None, None, None, None)?)
