33370989480f5f6d2faa78535e7e6180265e3a11
0xAA55 <weixiaobao213@qq.com>
2025-04-06 00:53:15 +0800

    实现把 `Vec<S>` 数组转换为 `tuple(S, S)` 数组，单声道会被复制成双声道。

--- src/encoders.rs
+++ src/encoders.rs
@@ -150,6 +150,19 @@ impl Encoder {
     }
 }
 
+pub fn stereo_to_tuples<S>(frames: &[Vec<S>]) -> Result<Vec<(S, S)>, AudioWriteError>
+where S: SampleType {
+    let mut tuples = Vec::<(S, S)>::with_capacity(frames.len());
+    for frame in frames.iter() {
+        match frame.len() {
+            1 => tuples.push((frame[0], frame[0])),
+            2 => tuples.push((frame[0], frame[1])),
+            other => return Err(AudioWriteError::InvalidArguments(format!("Channel number is {other}, can't turn to 2 tuples."))),
+        }
+    }
+    Ok(tuples)
+}
+
 // 样本类型缩放转换
 // 根据样本的存储值范围大小的不同，进行缩放使适应目标样本类型。
 fn sample_conv<S, D>(frame: &[S]) -> Vec<D>
