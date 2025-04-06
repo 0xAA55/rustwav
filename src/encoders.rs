b5bf4a9be6334a73b4df21a37050a791a096d7cf
0xAA55 <weixiaobao213@qq.com>
2025-04-06 17:40:33 +0800

    不再实现 `write_frame()`，因为 ADPCM 只支持 1、2 声道。

--- src/encoders.rs
+++ src/encoders.rs
@@ -521,77 +521,9 @@ where E: adpcm::AdpcmEncoder {
         Ok(())
     }
 
-    pub fn write_frame(&mut self, writer: &mut dyn Writer, frame: &Vec<i16>) -> Result<(), AudioWriteError> {
-        let mut sample_sent_l = false;
-        let mut sample_sent_r = false;
-        let feeder_l = || -> Option<i16> {
-            if sample_sent_l == false {
-                sample_sent_l = true;
-                Some(frame[0])
-            } else {
-                None
-            }
-        };
-        let feeder_r = || -> Option<i16> {
-            if sample_sent_r == false {
-                sample_sent_r = true;
-                Some(frame[1])
-            } else {
-                None
-            }
-        };
-        let receiver_l = |byte: u8|{ self.buffer_l.push(byte); };
-        let receiver_r = |byte: u8|{ self.buffer_r.push(byte); };
-        match frame.len() {
-            1 => {
-                self.encoder_l.encode(feeder_l, receiver_l)?;
-                self.samples_written += 1;
-
-                writer.write_all(&self.buffer_l)?;
-                self.bytes_written += self.buffer_l.len() as u64;
-                self.buffer_l.clear();
-
-                Ok(())
-            },
-            2 => {
-                self.encoder_l.encode(feeder_l, receiver_l)?;
-                self.encoder_r.encode(feeder_r, receiver_r)?;
-                self.samples_written += 1;
-                self.samples_written += 1;
-
-                self.flush_stereo(writer)?;
-                
-                Ok(())
-            }
-            other => Err(AudioWriteError::InvalidArguments(format!("Wrong channel number {other}"))),
         }
     }
 
-    pub fn write_multiple_frames(&mut self, writer: &mut dyn Writer, frames: &[Vec<i16>]) -> Result<(), AudioWriteError> {
-        let (lv, rv) = stereo_to_dual_mono(frames)?;
-        let (mut li, mut ri) = (lv.iter(), rv.iter());
-        let feeder_l = || -> Option<i16> { li.next().copied() };
-        let feeder_r = || -> Option<i16> { ri.next().copied() };
-        let receiver_l = |byte: u8|{ self.buffer_l.push(byte); };
-        let receiver_r = |byte: u8|{ self.buffer_r.push(byte); };
-        match (lv.len() > 0, rv.len() > 0) {
-            (true, false) => {
-                self.encoder_l.encode(feeder_l, receiver_l)?;
-                self.samples_written += lv.len() as u64;
-                writer.write_all(&self.buffer_l)?;
-                self.bytes_written += self.buffer_l.len() as u64;
-                self.buffer_l.clear();
-                Ok(())
-            },
-            (true, true) => {
-                self.encoder_l.encode(feeder_l, receiver_l)?;
-                self.encoder_r.encode(feeder_r, receiver_r)?;
-                self.samples_written += lv.len() as u64;
-                self.samples_written += rv.len() as u64;
-                self.flush_stereo(writer)?;
-                Ok(())
-            },
-            _ => panic!("The developer must have changed the behavior of the function `stereo_to_dual_mono()`, it should only feed the `left` channel for mono."),
         }
     }
 }
