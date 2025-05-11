495804e51f9463133572524945f3462079274352
0xAA55 <weixiaobao213@qq.com>
2025-05-11 20:11:39 +0800

    实现 `seal_packet()` 将当前的 `OggPacket` 封包，然后进行下一个 `OggPacket` 的打包。原先这个行为是用 `Write::flush()` 实现的，但是思考了一下觉得不合适，需要单独的封包函数。

--- src/ogg.rs
+++ src/ogg.rs
@@ -313,6 +313,20 @@ where
 		self.cur_packet = OggPacket::new(self.stream_id, OggPacketType::BeginOfStream, 0);
 		self.granule_position = 0;
 	}
+
+	pub fn seal_packet(&mut self, granule_position: u64, is_end_of_stream: bool) -> io::Result<()> {
+		self.granule_position = granule_position;
+		self.cur_packet.granule_position = self.granule_position;
+		let packed = if is_end_of_stream {
+			self.cur_packet.packet_type = OggPacketType::EndOfStream;
+			mem::take(&mut self.cur_packet).to_bytes()
+		} else {
+			mem::replace(&mut self.cur_packet, OggPacket::new(self.stream_id, OggPacketType::Continuation, self.packet_index)).to_bytes()
+		};
+		self.writer.write_all(&packed)?;
+		self.packet_index += 1;
+		Ok(())
+	}
 }
 
 impl<W> Write for OggStreamWriter<W>
