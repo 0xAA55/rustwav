f13f35a267d5100370c94eca2e11debfd07331df
0xAA55 <weixiaobao213@qq.com>
2025-05-13 15:49:42 +0800

    使用 `read_slice!()` 和 `read_slice_4!()` 读取 slice，减少开发失误发生率。

--- src/vorbis.rs
+++ src/vorbis.rs
@@ -693,21 +693,23 @@ pub struct VorbisIdentificationHeader {
 }
 
 impl VorbisIdentificationHeader {
-    pub fn parse(mut data: &[u8]) -> Result<Self, AudioError> {
-        let ident = &data[..7]; data = &data[7..];
+    pub fn parse(data: &[u8]) -> Result<Self, AudioError> {
+        let mut data = data;
+        let ident = read_slice!(data, 7);
         if ident != b"\x01vorbis" {
             Err(AudioError::InvalidData(format!("Not a Vorbis identification header, the header type is {}, the string is {}", ident[0], String::from_utf8_lossy(&ident[1..]))))
         } else {
-            let version = u32::from_le_bytes(data[0..4].try_into().unwrap()); data = &data[4..];
-            let channels = data[0]; data = &data[1..];
-            let sample_rate = u32::from_le_bytes(data[0..4].try_into().unwrap()); data = &data[4..];
-            let bitrate_upper = u32::from_le_bytes(data[0..4].try_into().unwrap()); data = &data[4..];
-            let bitrate_nominal = u32::from_le_bytes(data[0..4].try_into().unwrap()); data = &data[4..];
-            let bitrate_lower = u32::from_le_bytes(data[0..4].try_into().unwrap()); data = &data[4..];
-            let bs_1 = data[0] & 0x0F;
-            let bs_2 = data[0] >> 4;
-            let block_size = [1 << bs_1, 1 << bs_2]; data = &data[1..];
-            let framing_flag = data[0] & 1 == 1; data = &data[1..];
+            let version = u32::from_le_bytes(read_slice_4!(data));
+            let channels = read_slice!(data, 1)[0];
+            let sample_rate = u32::from_le_bytes(read_slice_4!(data));
+            let bitrate_upper = u32::from_le_bytes(read_slice_4!(data));
+            let bitrate_nominal = u32::from_le_bytes(read_slice_4!(data));
+            let bitrate_lower = u32::from_le_bytes(read_slice_4!(data));
+            let bs = read_slice!(data, 1)[0];
+            let bs_1 = bs & 0x0F;
+            let bs_2 = bs >> 4;
+            let block_size = [1 << bs_1, 1 << bs_2];
+            let framing_flag = read_slice!(data, 1)[0] & 1 == 1;
             if !data.is_empty()
             || sample_rate < 1
             || channels < 1
